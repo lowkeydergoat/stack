234fa3b04ea7d8c84fdd9caf1e72bc5af799adc1
stefan <skordov@student.tgm.ac.at>
2025-01-22 14:30:25 +0100

    JAVADOC

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Stack","l":"list()"},{"p":"<Unnamed>","c":"Stack","l":"peek()"},{"p":"<Unnamed>","c":"Stack","l":"pop()"},{"p":"<Unnamed>","c":"Stack","l":"push(T)"},{"p":"<Unnamed>","c":"Stack","l":"Stack()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Stack","l":"Stack(int)","u":"%3Cinit%3E(int)"}];updateSearchResults();
